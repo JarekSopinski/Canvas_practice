f73bfef1eb00649a1ce33921fa25705e40d95dde
JarekSopinski <jarek.sopinski@gmail.com>
2018-02-07 13:30:27 +0100

    every new particle can check for collisions with other particles

--- 08_collisions_part2/canvas08.js
+++ 08_collisions_part2/canvas08.js
@@ -50,20 +50,38 @@ function Particle(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.color = color
+    this.color = color;
+
+    this.update = particles => { // particles are passed as parameter to update method
+
+        this.draw();
+
+        // we check for collisions between one particle and all the others:
+        for (let i = 0; i < particles.length; i++) {
+
+            if (this === particles[i]) continue;
+            // we don't want to compare a particle to itself, so we skip using continue;
+
+            if (distance(this.x, this.y, particles[i].x, particles[i].y) - this.radius * 2 < 0) {
+                // on every loop iteration we get distance between this particle and all the surrounding particles
+
+                console.log('has collided');
+
+            }
+
+        }
+
+    };
+
+    this.draw = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = this.color;
+        c.stroke();
+        c.closePath()
+    };
+
 }
-
-Object.prototype.update = function() {
-    this.draw()
-};
-
-Object.prototype.draw = function() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.strokeStyle = this.color;
-    c.stroke();
-    c.closePath()
-};
 
 // Implementation
 let particles;
@@ -89,7 +107,7 @@ function init() {
 
                 // for every newly generated particle, location will be compared to previous particles
                 // to check if collision happens
-                if ( distance(x, y, particles[j].x, particles[j].y) - radius * 2 < 0) {
+                if ( distance(x, y, particles[j].x, particles[j].y) - radius * 2 < 0 ) {
 
                     // if collision happens, we want to recalculate particle's coordinates:
                     // this process will repeat itself until new particles does not overlap with previous ones
@@ -115,7 +133,7 @@ function animate() {
     c.clearRect(0, 0, canvas.width, canvas.height);
 
     particles.forEach(particle => {
-    particle.update();
+    particle.update(particles);
     });
 
 };
